c32f43bf7f6a4dcc46eee422212e3485274bf119
LUNEi3 <niranon49@gmail.com>
2025-04-30 15:47:06 +0700

    Add connection to database

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Test merge first_branch to main")
+from sqlite3 import *
+
+def connection():
+    global conn, cursor
+    conn = connect("database/project.db")
+    cursor = conn.cursor()
+
+connection()
