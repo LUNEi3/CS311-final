b1f38a963e1ff476ed1a6712e51ca968c2ac355b
LUNEi3 <niranon49@gmail.com>
2025-05-10 16:45:30 +0700

    Refactor note feature by organzing Frame()

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def mainwindow():
 
 # Note
 def notePage():
-    global foundFrame, notFoundFrame
+    global listNoteFrame
     loginFrame.grid_forget()
     profileFrame.grid_forget()
     todolistFrame.grid_forget()
@@ -40,46 +40,49 @@ def notePage():
     result = getNote()
     noteFrame.rowconfigure(0, weight=1)
     noteFrame.rowconfigure((1,2,3), weight=2)
-    noteFrame.columnconfigure((0), weight=3)
-    noteFrame.columnconfigure((1), weight=1)
+    noteFrame.columnconfigure(0, weight=1)
     noteFrame.grid(row=0, column=1, sticky="news")
     Label(noteFrame, bg="#547792", fg="black", text="Note", font="Garamond 26 bold").grid(row=0, column=0, columnspan=2, sticky="news")
 
-    foundFrame = Frame(noteFrame, bg="white")
-    notFoundFrame = Frame(noteFrame, bg="white")
+    # Local content frame
+    listNoteFrame.rowconfigure((0,1,2), weight=1)
+    listNoteFrame.columnconfigure(0, weight=5)
+    listNoteFrame.columnconfigure(1, weight=1)
+    listNoteFrame.grid(row=1, rowspan=3, column=0, sticky="news")
+    
+    foundFrame = Frame(listNoteFrame, bg="white")
+    notFoundFrame = Frame(listNoteFrame, bg="white")
     spy = IntVar()
     if result == 0:
         foundFrame.grid_forget()
         Label(notFoundFrame, bg="white", fg="black", text="No note here").pack(expand=TRUE)
-        notFoundFrame.grid(row=1, rowspan=3, column=0, sticky="news")
+        notFoundFrame.grid(row=0, rowspan=3, column=0, sticky="news")
     else:
         notFoundFrame.grid_forget()
         foundFrame.rowconfigure(0, weight=1)
         foundFrame.columnconfigure((0,1), weight=1)
-        foundFrame.grid(row=1, rowspan=3, column=0, sticky="news", padx=20, pady=20)
+        foundFrame.grid(row=0, rowspan=3, column=0, sticky="news", padx=20, pady=20)
         for item in result:
             Radiobutton(foundFrame, bg="white", fg="black", text=f"{item[2]}", font="Garamond 18", variable=spy, value=item[0], justify="left").pack(anchor="w", padx=10)
 
-    Button(noteFrame, bg="white", fg="black", text="Show note", width=10, command=lambda: showNote(spy.get())).grid(row=1, column=1, pady=20, sticky="s", ipady=10)
-    Button(noteFrame, bg="white", fg="black", text="Add note", width=10).grid(row=2, column=1, pady=20, ipady=10)
-    Button(noteFrame, bg="white", fg="black", text="Edit note", width=10).grid(row=3, column=1, pady=20, sticky="n", ipady=10)
+    Button(listNoteFrame, bg="white", fg="black", text="Show note", width=10, command=lambda: showNote(spy.get())).grid(row=0, column=1, pady=20, sticky="s", ipady=10)
+    Button(listNoteFrame, bg="white", fg="black", text="Add note", width=10).grid(row=1, column=1, pady=20, ipady=10)
+    Button(listNoteFrame, bg="white", fg="black", text="Edit note", width=10).grid(row=2, column=1, pady=20, sticky="n", ipady=10)
 
 
 def showNote(id):
     print(f"ID: {id}")
-    noteFrame.grid_forget()
-    curNoteFrame.rowconfigure(0, weight=1)
-    curNoteFrame.rowconfigure((1,2,3), weight=2)
-    curNoteFrame.columnconfigure((0), weight=3)
+    listNoteFrame.grid_forget()
+    curNoteFrame.rowconfigure((0,1,2), weight=1)
+    curNoteFrame.columnconfigure((0), weight=5)
     curNoteFrame.columnconfigure((1), weight=1)
-    curNoteFrame.grid(row=0, column=1, sticky="news")
-    Label(curNoteFrame, bg="#547792", fg="black", text="Note", font="Garamond 26 bold").grid(row=0, column=0, columnspan=2, sticky="news")
-    Button(curNoteFrame, bg="white", fg="black", text="Back", command=notePage).grid(row=1, column=1, pady=25, ipady=10, ipadx=20)
+    curNoteFrame.grid(row=1, rowspan=3, column=0, sticky="news")
+
     contentFrame = Frame(curNoteFrame, bg="white")
     contentFrame.rowconfigure(0, weight=1)
     contentFrame.rowconfigure(1, weight=5)
     contentFrame.columnconfigure(0, weight=1)
-    contentFrame.grid(row=1, rowspan=3, column=0, sticky="news", padx=20, pady=20)
+    contentFrame.grid(row=0, rowspan=3, column=0, sticky="news", padx=20, pady=20)
 
     # Fetch content
     sql = "SELECT * FROM notes WHERE id=?"
@@ -91,6 +94,15 @@ def showNote(id):
     else:
         Label(contentFrame, bg="white", fg="black", text=f"Please select note.", font="Garamond 26 bold").grid(row=0, column=0, sticky="news")
 
+    Button(curNoteFrame, bg="white", fg="black", text="Back", command=notePage).grid(row=0, column=1, pady=25, ipady=10, ipadx=20)
+
+
+def addNote():
+    ...
+
+
+def editNote():
+    ...
 
 
 def getNote():
@@ -114,7 +126,8 @@ loginFrame = Frame(root, bg="black")
 menuFrame = Frame(root, bg="#ECEFCA")
 profileFrame = Frame(root, bg="black")
 noteFrame = Frame(root, bg="#94B4C1")
-curNoteFrame = Frame(root, bg="#94B4C1")
+curNoteFrame = Frame(noteFrame, bg="#94B4C1")
+listNoteFrame = Frame(noteFrame, bg="#94B4C1")
 todolistFrame = Frame(root, bg="black")
 
 # Config menuFrame 
