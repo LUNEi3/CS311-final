fda8bf33cb821d0dc7309280b79447dd48addb2e
LUNEi3 <niranon49@gmail.com>
2025-05-12 20:03:27 +0700

    Complete note feature(no bug tested)

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from sqlite3 import *
 from tkinter import *
 from tkinter import messagebox
-
+import textwrap
 
 def connection():
     global conn, cursor
@@ -11,7 +11,7 @@ def connection():
 
 def mainwindow():
     root = Tk()
-    w = 1100
+    w = 1500
     h = 800
     x = root.winfo_screenwidth()/2 - w/2
     y = root.winfo_screenheight()/2 - h/2
@@ -110,8 +110,7 @@ def register_account():
 def go_back_to_login():
     register.grid_forget()
     loginFrame.grid(sticky='news')
-    
-    
+
 # Profile
 def view_profile(username):
     cursor.execute("SELECT * FROM profiles WHERE username=?", (username,))
@@ -128,7 +127,196 @@ def view_profile(username):
     Button(profileFrame, text="Back to menu").pack(pady=20)
 
 # Note
+def notePage():
+    global listNoteFrame, title
+    loginFrame.grid_forget()
+    profileFrame.grid_forget()
+    todolistFrame.grid_forget()
+    curNoteFrame.grid_forget()
+    addNoteFrame.grid_forget()
+    editNoteFrame.grid_forget()
+    result = getNotes()
+    noteFrame.rowconfigure(0, weight=1)
+    noteFrame.rowconfigure((1,2,3), weight=2)
+    noteFrame.columnconfigure(0, weight=1)
+    noteFrame.grid(row=0, column=1, sticky="news")
+    
+    # Title must be defind at first frame
+    title = Label(noteFrame, bg="#547792", fg="black", text="Note", font="Garamond 26 bold")
+    title.grid(row=0, column=0, columnspan=2, sticky="news")
 
+    # Local content frame
+    listNoteFrame.rowconfigure((0,1,2), weight=1)
+    listNoteFrame.columnconfigure(0, weight=5)
+    listNoteFrame.columnconfigure(1, weight=1)
+    listNoteFrame.grid(row=1, rowspan=3, column=0, sticky="news")
+    
+    foundFrame = Frame(listNoteFrame, bg="white")
+    notFoundFrame = Frame(listNoteFrame, bg="white")
+    spy = IntVar()
+    if result == 0:
+        foundFrame.grid_forget()
+        Label(notFoundFrame, bg="white", fg="black", text="No note here").pack(expand=TRUE)
+        notFoundFrame.grid(row=0, rowspan=3, column=0, sticky="news")
+    else:
+        notFoundFrame.grid_forget()
+        foundFrame.rowconfigure(0, weight=1)
+        foundFrame.columnconfigure((0,1), weight=1)
+        foundFrame.grid(row=0, rowspan=3, column=0, sticky="news", padx=20, pady=20)
+        for item in result:
+            Radiobutton(foundFrame, bg="white", fg="black", text=f"{item[2]}", font="Garamond 18", variable=spy, value=item[0], justify="left").pack(anchor="w", padx=10)
+
+    Button(listNoteFrame, bg="white", fg="black", text="Show note", width=10, command=lambda: showNote(spy.get())).grid(row=0, column=1, pady=20, sticky="s", ipady=10)
+    Button(listNoteFrame, bg="white", fg="black", text="Add note", width=10, command=addNote).grid(row=1, column=1, pady=20, ipady=10)
+    Button(listNoteFrame, bg="white", fg="black", text="Edit note", width=10, command=lambda: editNote(spy.get())).grid(row=2, column=1, pady=20, sticky="n", ipady=10)
+
+
+def showNote(id):
+    listNoteFrame.grid_forget()
+    addNoteFrame.grid_forget()
+    editNoteFrame.grid_forget()
+    curNoteFrame.rowconfigure((0,1,2), weight=1)
+    curNoteFrame.columnconfigure((0), weight=5)
+    curNoteFrame.columnconfigure((1), weight=1)
+    curNoteFrame.grid(row=1, rowspan=3, column=0, sticky="news")
+
+    contentFrame = Frame(curNoteFrame, bg="white")
+    contentFrame.rowconfigure(0, weight=1)
+    contentFrame.rowconfigure(1, weight=5)
+    contentFrame.columnconfigure(0, weight=1)
+    contentFrame.grid(row=0, rowspan=3, column=0, sticky="news", padx=20, pady=20)
+
+    # Fetch content
+    sql = "SELECT * FROM notes WHERE id=?"
+    cursor.execute(sql, [id])
+    result = cursor.fetchone()
+    if result:
+        Label(contentFrame, bg="white", fg="black", text=f"Title: {result[2]}", font="Garamond 26 bold").grid(row=0, column=0, sticky="news")
+        Label(contentFrame, bg="white", fg="black", text=f"{result[3]}").grid(row=1, column=0, sticky="nw", padx=20, pady=20)
+    else:
+        Label(contentFrame, bg="white", fg="black", text=f"Please select note.", font="Garamond 26 bold").grid(row=0, column=0, sticky="news")
+
+    Button(curNoteFrame, bg="white", fg="black", text="Back", command=notePage).grid(row=0, column=1, pady=25, ipadx=20, ipady=10)
+
+
+def addNote():
+    listNoteFrame.grid_forget()
+    curNoteFrame.grid_forget()
+    editNoteFrame.grid_forget()
+    addNoteFrame.rowconfigure((0,1,2), weight=1)
+    addNoteFrame.columnconfigure((0,1), weight=1)
+    addNoteFrame.grid(row=1, rowspan=3, column=0, sticky="news")
+
+    title["text"] = "Add Note"
+
+    # Local content frame
+    formFrame = Frame(addNoteFrame, bg="white")
+    formFrame.rowconfigure((0,1), weight=1)
+    formFrame.columnconfigure((0,1), weight=1)
+    formFrame.grid(row=0, rowspan=2, column=0, columnspan=2, sticky="news", padx=20, pady=20)
+
+    Label(formFrame, bg="white", fg="black", text="Title:").grid(row=0, column=0, sticky="es", padx=20, pady=20)
+    titleEntry = Entry(formFrame, bg="white", fg="black", width=20)
+    titleEntry.grid(row=0, column=1, sticky="ws", padx=20, pady=20)
+    Label(formFrame, bg="white", fg="black", text="Content:").grid(row=1, column=0, sticky="en", padx=20, pady=20)
+    contentEntry = Text(formFrame, bg="white", fg="black", width=20, height=10)
+    contentEntry.grid(row=1, column=1, sticky="wn", padx=20, pady=20)
+
+    Button(addNoteFrame, bg="white", fg="black", text="Back", width=20, command=notePage).grid(row=2, column=0, padx=20, pady=25, ipadx=20, ipady=10, sticky="e")
+    Button(addNoteFrame, bg="white", fg="black", text="Add", width=20, 
+            command=lambda: addNoteClick([titleEntry.get(), contentEntry.get("1.0", "end-1c")])
+            ).grid(row=2, column=1, padx=20, pady=25, ipadx=20, ipady=10, sticky="w")
+    
+
+def addNoteClick(data):
+    if data[0] == "" or data[1] == "":
+        messagebox.showwarning("Admin", "Please fill in all the data first")
+    else:
+        title = data[0].strip()
+        content = data[1].strip()
+        sql = f"INSERT INTO notes VALUES (null, ?, ?, ?)"
+        cursor.execute(sql, [USER, title, content])
+        conn.commit()
+        messagebox.showinfo("Admin", "Add note successfully")
+        notePage()
+
+
+def editNote(id):
+    if id == 0:
+        messagebox.showwarning("Admin", "Please select note.")
+        notePage()
+        return
+    else:
+        sql = "SELECT * FROM notes WHERE id=?"
+        cursor.execute(sql, [id])
+        result = cursor.fetchone()
+        if result:
+            formFrame = Frame(editNoteFrame, bg="white")
+            titleEntry = Entry(formFrame, bg="white", fg="black", width=20)
+            contentEntry = Text(formFrame, bg="white", fg="black", width=20, height=10)
+            titleEntry.insert(0, result[2])
+            contentEntry.insert("1.0", result[3])
+        else:
+            messagebox.showwarning("Admin", "Something went wrong on fetching data from editNote()")
+            print(f"ERROR on editNote(): ID is {id}. Is that exist in notes table?")
+            notePage()
+            return
+
+    listNoteFrame.grid_forget()
+    curNoteFrame.grid_forget()
+    addNoteFrame.grid_forget()
+    editNoteFrame.rowconfigure((0,1,2), weight=1)
+    editNoteFrame.columnconfigure((0,1,2), weight=1)
+    editNoteFrame.grid(row=1, rowspan=3, column=0, sticky="news")
+    
+    title["text"] = "Edit Note"
+
+    # Local content frame
+    formFrame.rowconfigure((0,1), weight=1)
+    formFrame.columnconfigure((0,1), weight=1)
+    formFrame.grid(row=0, rowspan=2, column=0, columnspan=3, sticky="news", padx=20, pady=20)
+
+    Label(formFrame, bg="white", fg="black", text="Title:").grid(row=0, column=0, sticky="es", padx=20, pady=20)
+    titleEntry.grid(row=0, column=1, sticky="ws", padx=20, pady=20)
+    Label(formFrame, bg="white", fg="black", text="Content:").grid(row=1, column=0, sticky="en", padx=20, pady=20)
+    contentEntry.grid(row=1, column=1, sticky="wn", padx=20, pady=20)
+
+    Button(editNoteFrame, bg="white", fg="black", text="Back", width=15, command=notePage).grid(row=2, column=0, padx=5, pady=25, ipadx=15, ipady=10)
+    Button(editNoteFrame, bg="white", fg="black", text="Update", width=15, 
+           command=lambda: updateNote(id, [titleEntry.get(), contentEntry.get("1.0", "end-1c")])
+           ).grid(row=2, column=1, padx=5, pady=25, ipadx=15, ipady=10)
+    Button(editNoteFrame, bg="white", fg="black", text="Delete", width=15, command=lambda: deletNote(id)).grid(row=2, column=2, padx=5, pady=25, ipadx=15, ipady=10)
+
+
+def updateNote(id, data):
+    isUpdate =  messagebox.askyesno("Admin", "Are you really want to update?")
+    if isUpdate:
+        sql = "UPDATE notes SET title=?, content=? WHERE id=?"
+        cursor.execute(sql, [data[0], data[1], id])
+        conn.commit()
+        messagebox.showinfo("Admin", "Update successfully")
+        notePage()
+
+
+def deletNote(id):
+    isDelete =  messagebox.askyesno("Admin", "Are you really want to delete?")
+    if isDelete:
+        sql = "DELETE FROM notes WHERE id=?"
+        cursor.execute(sql, [id])
+        conn.commit()
+        messagebox.showinfo("Admin", "Delete successfully")
+        notePage()
+
+
+def getNotes():
+    sql = "SELECT * FROM notes WHERE username=?"
+    cursor.execute(sql, [USER])
+    result = cursor.fetchall()
+    if result:
+        return result
+    else:
+        print("No result")
+        return 0
 
 # To-do List
 
@@ -140,7 +328,11 @@ root = mainwindow()
 loginFrame = Frame(root, bg="black")
 menuFrame = Frame(root, bg="#ECEFCA")
 profileFrame = Frame(root, bg="black")
-noteFrame = Frame(root, bg="black")
+noteFrame = Frame(root, bg="#94B4C1")
+curNoteFrame = Frame(noteFrame, bg="#94B4C1")
+listNoteFrame = Frame(noteFrame, bg="#94B4C1")
+addNoteFrame = Frame(noteFrame, bg="#94B4C1")
+editNoteFrame = Frame(noteFrame, bg="#94B4C1")
 todolistFrame = Frame(root, bg="black")
 top = Frame(root,bg='#493628')
 top2 = Frame(root,bg='#493628')
